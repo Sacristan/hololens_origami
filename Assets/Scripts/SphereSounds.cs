8617d5655fdf911e8e49fc5b14a48b072dc150ac
Girts Kesteris <girts.kesteris@riga.orangelv.com>
2017-02-22 17:51:42 +0200

    Initial Commit

---
+++ Assets/Scripts/SphereSounds.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SphereSounds : MonoBehaviour
+{
+    AudioSource audioSource = null;
+    AudioClip impactClip = null;
+    AudioClip rollingClip = null;
+
+    bool rolling = false;
+
+    void Start()
+    {
+        // Add an AudioSource component and set up some defaults
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialize = true;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.dopplerLevel = 0.0f;
+        audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+        audioSource.maxDistance = 20f;
+
+        // Load the Sphere sounds from the Resources folder
+        impactClip = Resources.Load<AudioClip>("Impact");
+        rollingClip = Resources.Load<AudioClip>("Rolling");
+    }
+
+    // Occurs when this object starts colliding with another object
+    void OnCollisionEnter(Collision collision)
+    {
+        // Play an impact sound if the sphere impacts strongly enough.
+        if (collision.relativeVelocity.magnitude >= 0.1f)
+        {
+            audioSource.clip = impactClip;
+            audioSource.Play();
+        }
+    }
+}
